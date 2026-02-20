742e87e4f7a57a55a9e9c2e5e9e993ea9eb4396c
serraguti <pacoserranox@gmail.com>
2026-02-20 19:35:38 +0100

    up

--- webinformacion/formularios/views.py
+++ webinformacion/formularios/views.py
@@ -9,18 +9,54 @@ def ejemplo(request):
     return render(request, "paginas/ejemplo.html")
 
 def saludo(request):
+    listaelementos = [
+        {
+            "index": 0,
+            "titulo": "Spiderman",
+            "imagen": "https://elcoleccionistacomics.com/60266-medium_default/spiderman-de-todd-mcfarlane-vol1-de-6.jpg"
+        },
+        {
+            "index": 1,
+            "titulo": "Spawn",
+            "imagen": "https://m.media-amazon.com/images/I/91hZO1pjAoL._AC_UF1000,1000_QL80_.jpg"
+        },
+        {
+            "index": 2,
+            "titulo": "Wolverine",
+            "imagen": "https://www.kamekame.es/53918-large_default/marvel-omnibus-dinastia-de-x-potencias-de-x-comic.jpg"
+        },
+        {
+            "index": 3,
+            "titulo": "Wolverine",
+            "imagen": "https://www.kamekame.es/53918-large_default/marvel-omnibus-dinastia-de-x-potencias-de-x-comic.jpg"
+        },
+        {
+            "index": 4,
+            "titulo": "Asterix y Obelix",
+            "imagen": "https://comicsbarcelona.com/wp-content/uploads/2015/11/127913-Asterix-2.-La-Hoz-de-Oro.jpg"
+        } 
+               
+              
+    ]
     #Siempre con un POST debemos preguntar
     if ('cajanombre' in request.POST):
         #aqui tenemos caja con valor
         dato = request.POST["cajanombre"]
+        valor = int(request.POST["select"])
+        elem = listaelementos[valor]
         #Enviamos informacion
         informacion = {
-            "nombre": dato
+            "nombre": dato,
+            "lista": listaelementos,
+            "elemento": elem
         }
         return render(request, "paginas/saludo.html", informacion)
     else:
         #No enviamos informacion
-        return render(request, "paginas/saludo.html")
+        informacion = {
+            "lista": listaelementos
+        }
+        return render(request, "paginas/saludo.html", informacion)
 
 def SumarNumeros(request):
     if ('cajanum1' in request.POST):
